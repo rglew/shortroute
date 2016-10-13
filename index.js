b20cc6e34c960cb37d54be7e924c24505c904eda
Richard Glew <richard@literacyplanet.com>
2016-10-13 13:52:26 +1100

    works with provided filename

--- index.js
+++ index.js
@@ -4,11 +4,19 @@
 var program = require('commander');
 var fs = require('fs');
 
-var array = fs.readFileSync('input/graph.txt').toString().split(',');
+
+function LoadGraph(graph){
+
+var array = fs.readFileSync(graph).toString().split(',');
 var i = 0;
 for(i in array) {
     console.log(array[i].trim());
 }
+
+// Assuming file can be read OK, then it needs to be additionally validated.  return true if ok, otherwise error out
+
+};
+
  
 program
   .version('0.0.1')
@@ -17,4 +25,9 @@ program
   .option('-t, --test', 'Run the tests')
   .parse(process.argv);
 
-  console.log(program.args);
+
+//check for test, help first.  if either are true - do it and exit
+// use default graph if no graph specified, otherwise load and validate supplied graph text file
+
+if (program.load) LoadGraph(program.args.toString());
+
