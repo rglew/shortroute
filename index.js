c0e4103826ef9d80c19127bf82bf6b710bf662ca
Richard Glew <richard@literacyplanet.com>
2016-10-13 15:34:15 +1100

    updated pseudocode for solution

--- index.js
+++ index.js
@@ -15,7 +15,39 @@ for(i in array) {
 
 // Assuming file can be read OK, then it needs to be additionally validated.  return true if ok, otherwise error out
 
+// then need some helper functions to work out:
+
+
 };
+
+
+function CalcRoute(graph, route) {
+
+// pre-requisites
+
+// need a list of the unique de-duped set of vertices (so can create the right size 2d array initialised as infinity or a very large number)
+// need a list of the supplied directed graph weights (used to update the 2d array for steps 4&5 below)
+// then implement the below
+
+//pseudocode for Floyd Warshall goes here
+
+//1 let dist be a |V| × |V| array of minimum distances initialized to ∞ (infinity)
+//2 for each vertex v
+//3    dist[v][v] ← 0
+//4 for each edge (u,v)
+//5    dist[u][v] ← w(u,v)  // the weight of the edge (u,v)
+//6 for k from 1 to |V|
+//7    for i from 1 to |V|
+//8       for j from 1 to |V|
+//9          if dist[i][j] > dist[i][k] + dist[k][j] 
+//10             dist[i][j] ← dist[i][k] + dist[k][j]
+//11         end if
+
+// the result will be a 2d array (matrix) of minimum distances so the return value just needs to look up the route
+
+};
+
+
 
  
 program
